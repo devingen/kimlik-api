0afec71fdd22b73c8139a383fc6317fbc5fc3d89
Emir Luleci <emrullahluleci@gmail.com>
2022-02-10 00:04:49 +0000

    add metadata content field to saml config

--- model/saml-config.go
+++ model/saml-config.go
@@ -16,10 +16,12 @@ type SAMLConfig struct {
 	UpdatedAt *time.Time `json:"_updated,omitempty" bson:"_updated,omitempty"`
 	Revision  int        `json:"_revision,omitempty" bson:"_revision,omitempty"`
 
-	MetadataURL                 string   `json:"metadataURL" bson:"metadataURL,omitempty" validate:"required"`
-	AssertionConsumerServiceURL string   `json:"assertionConsumerServiceURL" bson:"assertionConsumerServiceURL,omitempty" validate:"required"`
-	AudienceURI                 string   `json:"audienceURI" bson:"audienceURI,omitempty" validate:"required"`
-	ServiceProviderIssuer       string   `json:"serviceProviderIssuer" bson:"serviceProviderIssuer,omitempty" validate:"required"`
+	// TODO require either
+	MetadataURL                 *string  `json:"metadataURL" bson:"metadataURL,omitempty" validate:"required"`
+	MetadataContent             *string  `json:"metadataContent" bson:"metadataContent,omitempty" validate:"required"`
+	AssertionConsumerServiceURL *string  `json:"assertionConsumerServiceURL" bson:"assertionConsumerServiceURL,omitempty" validate:"required"`
+	AudienceURI                 *string  `json:"audienceURI" bson:"audienceURI,omitempty" validate:"required"`
+	ServiceProviderIssuer       *string  `json:"serviceProviderIssuer" bson:"serviceProviderIssuer,omitempty" validate:"required"`
 	SAMLResponseValues          []string `json:"samlResponseValues" bson:"samlResponseValues,omitempty"`
 }
 
